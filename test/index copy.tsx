4d81cbffe5741dd8b4bdf6a246a50607b514a290
dhryshyn <dev.dmytroh@gmail.com>
2023-04-11 13:33:15 +0300

    Test commit

--- test/index copy.tsx
+++ test/index copy.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom-v5-compat";
 import React from 'react';
-console.log('tesdfsdfsdfst')
+console.log('tesdfsdfsdfs 1231312312')
 const test = () => {
   return (
     <div>test</div>
