fc32fd75a67ea793ba2bec3961c6930de0f9a568
dhryshyn <dev.dmytroh@gmail.com>
2023-04-20 16:54:58 +0300

    Test ahahah ahah :D :D

--- test/index copy.tsx
+++ test/index copy.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom-v5-compat';
+import { useLocation } from 'react-router-dom-v5-compat';
 import React from 'react';
 console.log('test');
 const test = () => {
